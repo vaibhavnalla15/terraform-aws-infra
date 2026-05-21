f4b4e3011485cda34967a6976fa17ef5069b89d7
Vaibhav <vaibhavnalla15@gmail.com>
2026-05-21 18:47:24 +0530

    Updated variables.tf & output.tf for vpc module

--- environments/dev/output.tf
+++ environments/dev/output.tf
@@ -7,3 +7,18 @@ output "bucket_arn" {
   description = "The ARN of the bucket"
   value       = module.tf_bucket.bucket_arn
 }
+
+# Outputs VPC ID from VPC module
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+# Outputs public subnet IDs from VPC module
+output "public_subnet_ids" {
+  value = module.vpc.public_subnet_ids
+}
+
+# Outputs private subnet IDs from VPC module
+output "private_subnet_ids" {
+  value = module.vpc.private_subnet_ids
+}
--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -12,3 +12,32 @@ variable "iam_user" {
   description = "IAM user name"
   type        = string
 }
+
+variable "vpc-cidr" {
+  description = "This is the value for VPC CIDR"
+  type        = string
+}
+
+variable "public-subnet-cidr" {
+  description = "List of public subnet cidrs"
+  type        = list(string)
+}
+
+variable "private-subnet-cidr" {
+  description = "List of private subnet cidrs"
+  type        = list(string)
+}
+
+variable "azs" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+variable "vpc-tags" {
+  description = "Tags for the projects"
+  type        = map(string)
+}
+
+variable "environment" {
+  type = string
+}
